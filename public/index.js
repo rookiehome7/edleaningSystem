77cc6d3c7c92bf6362bd12319edab4a708b58dff
Takdanai Jirawanichkul <takdanaijirawanichkul@Takdanais-MacBook-Air.local>
2018-11-22 18:12:41 +0700

    home004

--- public/index.js
+++ public/index.js
@@ -2,39 +2,44 @@ firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     // User is signed in.
     var user = firebase.auth().currentUser;
-    firebase.database().ref('/users/'+user.uid+'/role').once('value', (snapshot) => {
-      if (snapshot.val() == "Teacher"){
+    firebase.database().ref('/users/'+user.uid).once('value', (snapshot) => {
+
+      var firstname = snapshot.val().firstname;
+      var lastname = snapshot.val().lastname;
+      document.getElementById("user_name").innerHTML = "Hello, " + firstname + " " + lastname;
+
+      if (snapshot.val().role == "Teacher"){
         document.getElementById("user_div_teacher").style.display = "block";
         document.getElementById("user_div_student").style.display = "none";
+
+        document.getElementById("nav_div_teacher").style.display = "block";
+        document.getElementById("nav_div_teacher1").style.display = "block";
+        document.getElementById("nav_div_teacher2").style.display = "block";
+        document.getElementById("nav_div_student").style.display = "none";
         //alert("Teacher");
       }
-      else if (snapshot.val() == "Student") {
+      else if (snapshot.val().role == "Student") {
         document.getElementById("user_div_teacher").style.display = "none";
         document.getElementById("user_div_student").style.display = "block";
+
+        document.getElementById("nav_div_teacher").style.display = "none";
+        document.getElementById("nav_div_teacher1").style.display = "none";
+        document.getElementById("nav_div_teacher2").style.display = "none";
+        document.getElementById("nav_div_student").style.display = "block";
         //alert("Student");
       }
       else {
-        alert(snapshot.exists() ? snapshot.val() : "no user role");
+        alert(snapshot.exists() ? snapshot.val().role : "no user role");
       }
-      //alert(snapshot.exists() ? snapshot.val() : "no user role");
     }, (error) => {
       if(error) {
       }
     });
-
-    if(user != null){
-      var email_id = user.email;
-      document.getElementById("user_para").innerHTML = "Hello, " + email_id;
-      document.getElementById("user_para1").innerHTML = "Hello, " + email_id;
-    }
   } 
   else {
     window.location = "login.html";
   }
 });
-
-
-
 
 function login(){
   var userEmail = document.getElementById("email_field").value;
